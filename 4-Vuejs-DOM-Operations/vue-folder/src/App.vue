b2c3f16ef9d3e8644432fa33c5b79942931202f5
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2024-05-16 13:55:49 -0600

    9. Script tags definition and basic ussage notes added

--- 4-Vuejs-DOM-Operations/vue-folder/src/App.vue
+++ 4-Vuejs-DOM-Operations/vue-folder/src/App.vue
@@ -1,9 +1,16 @@
+<!-- HTML -->
 <!-- template tags are use for structuring the Vue.js components -->
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
+<!-- JavaScript -->
+<!-- script tags provides functionallity to the component -->
+<!-- the script tags process and display data -->
+<!-- you can add some event handling too -->
+<!-- ROUTER DEFINITION -->
+<!-- helps with the navigation through the SPA (Vue.js app) -->
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
@@ -15,6 +22,8 @@ export default {
 }
 </script>
 
+<!-- CSS -->
+<!-- Styling components with HTML and CSS, as always -->
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
