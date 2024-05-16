b64337429ba94b8cf180eb99c621ee1027551777
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2024-05-16 11:31:28 -0600

    5. Creating and styling first custom component with scoped CSS styles

---
+++ 3-Introduction-to-Vue/vue-cli/src/components/Class.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="test">Class</div>
+</template>
+
+<script>
+export default {
+  name: 'TestClass',
+};
+</script>
+
+<style scoped>
+.test {
+    background-color: blue;
+}
+</style>
