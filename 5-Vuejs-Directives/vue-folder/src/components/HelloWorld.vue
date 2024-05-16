62848ece6f977b0337078986e7dc76be5ad30bcd
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2024-05-16 17:20:02 -0600

    19. V-html directive practice done, better than {{  }} method

---
+++ 5-Vuejs-Directives/vue-folder/src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- This one is wrapped by b tags -->
+  <div>{{ myContent }}</div>
+  <!-- This one don't -->
+  <div v-html="myContent"></div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  methods: {},
+  data() {
+    return {
+      myContent: "<b style='color:red;'>Hello my content</b>"
+    }
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
