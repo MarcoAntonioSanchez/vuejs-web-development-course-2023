4b087a338fd7b90f984b67b57066b8a1772f3f51
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2024-05-16 14:15:42 -0600

    11. V-bind functionallity practice done, making dynamic classes and ids

--- 4-Vuejs-DOM-Operations/vue-folder/src/components/HelloWorld.vue
+++ 4-Vuejs-DOM-Operations/vue-folder/src/components/HelloWorld.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h1>{{ name }}</h1>
-    <h1>{{ age }}</h1>
-    <h1>{{ gender }}</h1>
-    <h1>{{ "Hello" }}</h1>
+    <h2 :id="myH2">Hello</h2>
+    <div v-bind:id="myId">Hello my div: {{ myId }}</div>
+    <img v-bind:src="mySrc" alt="">
   </div>
 </template>
 
@@ -13,10 +12,10 @@ export default {
   name: 'HelloWorld',
   data() {
     return {
-      name: "Jhon",
-      age: 25,
-      gender: true,
-    }
+      myId: "divId",
+      myH2: "myH2",
+      mySrc: "https://img.freepik.com/premium-vector/raven-stylized-illustration-design-crow-logo-intricate-details_801948-7.jpg",
+    };
   },
   props: {
     msg: String
@@ -26,5 +25,10 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+#divId {
+  background-color: red;
+}
+#myH2 {
+  background-color: cadetblue;
+}
 </style>
