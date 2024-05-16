6fa7a2e1ddd6a2bb48d4d4e7549c1d121214be66
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2024-05-16 17:04:10 -0600

    18. Two way data binding practice done, using the v-model directive

--- 4-Vuejs-DOM-Operations/vue-folder/src/components/HelloWorld.vue
+++ 4-Vuejs-DOM-Operations/vue-folder/src/components/HelloWorld.vue
@@ -1,26 +1,41 @@
 <template>
   <div>
-    <!-- One way to bind a method on click in the button tag -->
-    <!-- <button v-on:click="handleClick">Click</button> -->
-    <!-- Alternative way to bind a method on click in the button tag -->
-    <!-- <button @click="handleClick">Click</button>
-    <h1 id="header">Header</h1> -->
-    <!-- <h1>{{ msg }}</h1> -->
-    <!-- <button @click="handleClick">Click</button> -->
-    <!-- Set 100 value as param for the handleClick2 function -->
-    <!-- <button @click="handleClick2(100)">Click</button> -->
-    <!-- Event modifier "prevent" to prevent page refresh on form submit -->
-    <!-- <form @submit.prevent="handleSubmit">
+    <!-- One way to bind a method on click in the button tag
+    <button v-on:click="handleClick">Click</button>
+
+    Alternative way to bind a method on click in the button tag
+    <button @click="handleClick">Click</button>
+    <h1 id="header">Header</h1>
+    <h1>{{ msg }}</h1>
+    <button @click="handleClick">Click</button>
+    
+    Set 100 value as param for the handleClick2 function
+    <button @click="handleClick2(100)">Click</button>
+    
+    Event modifier "prevent" to prevent page refresh on form submit
+    <form @submit.prevent="handleSubmit">
       <input type="text"><br>
       <button type="submit">Submit</button>
-    </form> -->
-    <!-- Event modifier "once" to only be able to click once and thats it -->
-    <!-- <button @click.once="onBtnClick">Inc</button>
-    <p>{{ number }}</p> -->
+    </form>
+    Event modifier "once" to only be able to click once and thats it
+    <button @click.once="onBtnClick">Inc</button>
+    <p>{{ number }}</p>
+
+    Addition result is shown in the browser
   <h1>2+5 {{ 2 + 6 }}</h1>
+  
+  String time is show in the browser
   <h1>2+5 {{ new Date().toLocaleTimeString() }}</h1>
+  
+  Uppercase pre defined JS function reflects in the browser
   <h1>Hi my name is {{ "Jane".toUpperCase() }}</h1>
+  
+  Custom function output is show in the browser too
   <h1>{{ sayHello() }}</h1>
+
+  Two way data binding example
+  <input v-model="text" type="text">
+  {{ text }} -->
   </div>
 </template>
 
@@ -47,7 +62,7 @@ export default {
   },
   data() {
     return {
-      number: 1,
+      text: "",
     };
   },
   props: {
