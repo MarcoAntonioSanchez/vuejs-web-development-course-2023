1ac631a871cb331736f88cd8637e1239f6ce955c
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2024-05-16 17:24:18 -0600

    20. V-text directive practice done, easy

--- 5-Vuejs-Directives/vue-folder/src/components/HelloWorld.vue
+++ 5-Vuejs-Directives/vue-folder/src/components/HelloWorld.vue
@@ -1,8 +1,17 @@
 <template>
-  <!-- This one is wrapped by b tags -->
+<!--
+  V-HTML
+  This one is wrapped by b tags
   <div>{{ myContent }}</div>
-  <!-- This one don't -->
+  This one don't
   <div v-html="myContent"></div>
+  <div>
+    V-TEXT
+    This one shows the "Hello, How are you. string"
+    <h1 v-text="myText"></h1>
+  </div>
+-->
+<div></div>
 </template>
 
 <script>
@@ -11,6 +20,7 @@ export default {
   methods: {},
   data() {
     return {
+      myText: "Hello, How are you.",
       myContent: "<b style='color:red;'>Hello my content</b>"
     }
   },
