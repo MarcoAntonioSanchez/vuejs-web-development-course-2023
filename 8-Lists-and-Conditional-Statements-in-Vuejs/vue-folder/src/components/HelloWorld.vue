5700704cff5612359d3e3eabbd5fbe147d2b3582
MarcoAntonioSanchez <marco.a.sanchez.dev@gmail.com>
2024-05-20 16:43:52 -0600

    34. Object rendering practice done, displaying index, key and value of an objectc

--- 8-Lists-and-Conditional-Statements-in-Vuejs/vue-folder/src/components/HelloWorld.vue
+++ 8-Lists-and-Conditional-Statements-in-Vuejs/vue-folder/src/components/HelloWorld.vue
@@ -1,38 +1,62 @@
 <template>
-  <!--
-    RENDERING LIST
-    Generating name's list by binding with v-for, using item's id as key
+
+    <!-- RENDERING LIST
+    Generating name's list by binding with v-for, using item's id as key -->
     <div v-for="item in myList" :key="item.id">
       {{ item.name }}
     </div>
-
-    Destructuring the data inside myList, same result as above
+    <br>
+    <!-- Destructuring the data inside myList, same result as above -->
     <div v-for="{ name, id } in myList" :key="id">
       {{ name }}
     </div>
-
-    USING CONDITIONAL EXPRESSIONS IN LISTS
+    <hr>
+    <!-- USING CONDITIONAL EXPRESSIONS IN LISTS -->
     <div v-for="{ name, id, isActive } in myList" :key="id">
-      If the attribute is true, HTML tag H1 is shown, otherwise it won't be displayed
+      <!-- If the attribute is true, HTML tag H1 is shown, otherwise it won't be displayed -->
       <h1 v-if="isActive" class="header">
         {{ name }}
       </h1>
     </div>
-    
-    SENDING DATA IN LIST TO FUNCTION
-    Using @click to run the myData function, allong with the name attribute passed
+    <hr>
+    <!-- SENDING DATA IN LIST TO FUNCTION
+    Using @click to run the myData function, allong with the name attribute passed -->
     <div @click="myData(name)" v-for="{ name, id, isActive } in myList" :key="id">
       <h1 v-if="isActive" class="header">
         {{ name }}
       </h1>
     </div>
-  -->
+    <hr>
+    <!-- DELETING AN ITEM FROM A LIST
+    { object-attributes } replaced by (item, index) and id replaced with index as key -->
+    <div v-for="(item, index) in myList" :key="index">
+      <!-- @click function to remove the item using splash method contained in the removeData function  -->
+      <h1 @click="removeData(index)" class="header">
+        {{ item.name }}
+      </h1>
+    </div>
+    <hr>
+    <!-- OBJECT RENDERING -->
+    
+    <!-- Example #1
+    New item displayed -->
+    <div v-for="item in user" :key="item">
+      {{ item }}
+    </div>
+    <br>
+    <!-- Example #2
+    V-bind to show both attributes: item (value) and key -->
+    <div v-for="(item,key) in user" v-bind:key="item">
+      {{ key }} - {{ item }}
+    </div>
+    <br>
+    <!-- Example #3
+    Adding index count to the displayed item -->
+    <div v-for="(item,key,index) in user" v-bind:key="item">
+      {{ index }} - {{ key }} - {{ item }}
+    </div>
 
-  <div v-for="(item, index) in myList" :key="index">
-    <h1 @click="removeData(index)" class="header">
-      {{ item.name }}
-    </h1>
-  </div>
+<div></div>
 </template>
 
 <script>
@@ -64,7 +88,12 @@ export default {
           name: "Raven",
           isActive: true,
         }
-      ]
+      ],
+      user: { // New object to render
+        name: "Vue.js",
+        text: "Framework",
+        date: new Date().toLocaleDateString(),
+      }
     }
   },
   props: {
